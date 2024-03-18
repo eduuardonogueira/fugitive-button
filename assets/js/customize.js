881e8e088b472ad1bcbe982037f2ba541262d325
EduardoNogueiradev <eduardocastroemp@gmail.com>
2024-03-17 22:32:44 -0300

    feat/ added imgur api and menu page

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -2,19 +2,38 @@ const data = {};
 const url = window.location.toString().replace("customize.html", "");
 
 const question = document.getElementById("question");
-question.addEventListener("input", () => {
-  getInputValue(question);
-});
+question.addEventListener("input", () => getInputValue(question));
 
 const answer = document.getElementById("answer");
-answer.addEventListener("input", () => {
-  getInputValue(answer);
-});
+answer.addEventListener("input", () => getInputValue(answer));
 
 const imageUrl = document.getElementById("image-url");
-imageUrl.addEventListener("input", () => {
-  getInputValue(imageUrl);
-});
+imageUrl.addEventListener("input", () => getInputValue(imageUrl));
+
+const inputFile = document.getElementById("file");
+
+inputFile.addEventListener("change", () => uploadImage(inputFile));
+
+function uploadImage(inputFile) {
+  const file = inputFile.files[0];
+
+  var formData = new FormData();
+
+  formData.append("image", file);
+
+  const HttpConfig = {
+    method: "POST",
+    headers: {
+      Authorization: "Client-ID 5624532fa15df26",
+    },
+    data: formData,
+  };
+
+  fetch("https://api.imgur.com/3/image/", HttpConfig).then((response) => {
+    console.log(response);
+    imageUrl.value = response.data.link;
+  });
+}
 
 const generatedUrl = document.getElementById("generated-url");
 
